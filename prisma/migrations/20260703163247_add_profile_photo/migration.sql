7ffc98d0ba44c1d15b3dcfc5e6554126d7588948
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-03 22:52:42 +0600

    add data

---
+++ prisma/migrations/20260703163247_add_profile_photo/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "profiles" ADD COLUMN     "profilePhoto" TEXT;
--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,10 @@ import cookieParser from "cookie-parser";
 import express, { Application,Request,Response } from "express";
 import cors from "cors";
 import config from "./config";
+import  httpStatus from "http-status";
 import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
+
 
 const app: Application = express();
 app.use(cors(
@@ -15,6 +18,63 @@ app.use(cors(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+
+app.post("/api/users/register", async (req: Request, res: Response) => {
+
+    const {name, email, password,profilePhoto} = req.body;
+
+    const isUserExist = await prisma.user.findUnique({
+        where: {
+            email: email,
+        },
+    }); 
+    if (isUserExist) {
+        throw new Error("User already exists");
+    }
+    const hashedPassword = await bcrypt.hash(password,Number(config.bcrypt_salt_rounds));
+
+    const createdUser = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+    
+        }
+    });
+
+    await prisma.profile.create({
+        data: {
+            userID: createdUser.id,
+            profilePhoto,   
+        }
+    });
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: createdUser.id,
+            email: createdUser.email || email,
+        },
+        omit:{
+            password: true,
+        },
+        include :{
+            profile: true,
+        }
+    });
+
+   
+
+    res.status(httpStatus.CREATED).json({ 
+        success: true,
+        status: httpStatus.CREATED,
+        message: "User registered successfully" ,
+        data: {
+            user
+        } 
+
+    });
+});
 
 
 
