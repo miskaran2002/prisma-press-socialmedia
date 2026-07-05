e6163ae8f7d07d16aeb41154cf7bf0afc0be54ee
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-05 14:07:42 +0600

    add cookies

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,13 +7,32 @@ import httpStatus from "http-status";
 
 const loginResult = catchAsync(async (req:Request, res:Response, next:NextFunction) => {
     const payload = req.body;
-    const user = await authService.loginUser(payload);
+    const {accessToken, refreshToken} = await authService.loginUser(payload);
+
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure:false,
+        sameSite: "none",
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+        
+    })
+
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure:false,
+        sameSite: "none",
+        maxAge: 24 * 60 * 60 * 1000 *7, // 7 day
+    })
+
+
+
 
     sendResponse(res, {
         success: true,
         status: httpStatus.OK,
         message: "User logged in successfully",
-        data: { user },
+        data: { accessToken, refreshToken },
     }); 
 
 
