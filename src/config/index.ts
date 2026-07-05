6ff5e704d554c7df26aa12b2565972e9a33694d8
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-05 12:35:45 +0600

    ok

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,15 +4,15 @@ import path from "path";
 
 
 
-dotenv.config({path: path.join(process.cwd(), ".env")});
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export default {
     port: process.env.PORT,
     database_url: process.env.DATABASE_URL,
     app_url: process.env.APP_URL,
-    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS ,
-   jwt_access_secret: process.env.JWT_ACCESS_SECRET as string,
-  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN as string,
-jwt_refresh_secret: process.env.JWT_REFRESH_SECRET as string,
-  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN as string
+    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+    jwt_access_secret: process.env.JWT_ACCESS_SECRET as string,
+    jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN as string,
+    jwt_refresh_secret: process.env.JWT_REFRESH_SECRET as string,
+    jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN as string
 }
