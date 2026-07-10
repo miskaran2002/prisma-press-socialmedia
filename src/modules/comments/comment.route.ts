38a2114767184caf7e1255e4776ea66f9b71265e
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-10 23:05:40 +0600

    create comment route

--- src/modules/comments/comment.route.ts
+++ src/modules/comments/comment.route.ts
@@ -1,40 +1,44 @@
 import { Router } from "express";
-import { commentController } from "./coment.controller";
-import { auth } from "../../mddlewires/auth";
 import { Role } from "../../../generated/prisma/enums";
+import { auth } from "../../mddlewires/auth";
+import { commentController } from "./coment.controller";
+
 
 const router = Router();
 
-router.post("/",
+router.post( 
+    "/",
     auth(Role.USER, Role.ADMIN, Role.AUTHOR),
-    commentController.createComment);
-
-router.get("/authors/:authorId", commentController.getCommentByCommentId);
-
+    commentController.createComment
+);
 
 router.get(
-    "/:commentId",
-    commentController.getCommentByCommentId
+    "/author/:authorId",
+    commentController.getCommentByAuthorId
+);
 
+router.get(
+    "/:postId",
+    commentController.getCommentByPostId
 );
 
 router.patch(
     "/:commentId",
     auth(Role.USER, Role.ADMIN, Role.AUTHOR),
     commentController.updateComment
+);
 
-)
-
-router.delete("/:commentId",
+router.delete(
+    "/:commentId",
     auth(Role.USER, Role.ADMIN, Role.AUTHOR),
-    commentController.deleteComment);
+    commentController.deleteComment
+);
 
 router.put(
     "/:commentId/moderate",
     auth(Role.ADMIN),
     commentController.moderateComment
-)
+);
+
 
 export const commentRoutes = router;
-
-
