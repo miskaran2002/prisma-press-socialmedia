6cb306ac60850519b9c4585af0d64f06497b4893
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-10 20:50:17 +0600

    commit -roleback

--- src/posts/post.service.ts
+++ src/posts/post.service.ts
@@ -43,51 +43,101 @@ const getAllPost =async()=>{
 const getPostById =async(postId: string)=>{
   
  
-await prisma.post.update({
-    where:{
-        id:postId
-    },
-    data:{
-        views:{
-            increment:1
-        }
-    },
+// await prisma.post.update({
+//     where:{
+//         id:postId
+//     },
+//     data:{
+//         views:{
+//             increment:1
+//         }
+//     },
     
- })
+//  })
 
- const post = await prisma.post.findUniqueOrThrow({
-    where:{
-        id:postId
-    },
-    include:{
-        author:{
-            omit:{
-                password:true
-            }
+//  const post = await prisma.post.findUniqueOrThrow({
+//     where:{
+//         id:postId
+//     },
+//     include:{
+//         author:{
+//             omit:{
+//                 password:true
+//             }
 
-        },
-        comments:{
-            where:{
-                status:CommentStatus.APPROVED
+//         },
+//         comments:{
+//             where:{
+//                 status:CommentStatus.APPROVED
                 
-            },
-            orderBy:{
-                createdAt:"desc"
-            }
-        },
-        _count:{
-            select:{
-                comments:true
+//             },
+//             orderBy:{
+//                 createdAt:"desc"
+//             }
+//         },
+//         _count:{
+//             select:{
+//                 comments:true
                 
-            }
+//             }
             
-        }
-    }
+//         }
+//     }
      
- })
+//  })
 
 
- return post
+//  return post
+
+const transactionResult = await prisma.$transaction(
+    async(tx)=>{
+        await tx.post.update({
+            where:{
+                id:postId
+            },
+            data:{
+                views:{
+                    increment:1
+                }
+            },
+
+         })
+         const post = await tx.post.findUniqueOrThrow({
+            where:{
+                id:postId
+            },
+            include:{
+                author:{
+                    omit:{
+                        password:true
+                    }
+
+                },
+                comments:
+                {
+                    where:{
+                        status:CommentStatus.APPROVED
+
+                    },
+                    orderBy:{
+                        createdAt:"desc"
+                    }
+                },
+                _count:{
+                    select:{
+                        comments:true
+
+                    }
+
+                }
+            }
+        })
+      return post  
+    }
+);
+return transactionResult
+
+
 }
 
 const updatePost =async(postId:string,payload:IupdatePostPayload,authorId:string,
