82ab44601168bfed67b4fb08c2c21dc10f686509
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-14 00:31:37 +0600

    added

--- src/modules/subscription/subscription.controller.ts
+++ src/modules/subscription/subscription.controller.ts
@@ -1,8 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utilities/catchAsync";
+import { subscriptionService } from "./subscription.service";
+import status from "http-status";
 
 const createCheckoutSession= catchAsync(async (req:Request, res:Response, next:NextFunction) => {
-    
+    const userId= req.user?.id
+    const result = await subscriptionService.createCheckoutSession(userId as string)
+    res.status(200).json({
+        success: true,
+        status: status.OK,
+        message: "Checkout Session Created Successfully",
+        data: result,
+        
+    })
 })
 
 export const subscriptionController = {
